f269f5133b086b8c1a28a87b64f0f3fb9c6ea8cf
Jerald Bianan <jeraldbianan@gmail.com>
2024-08-18 12:25:05 +0800

    added prefix and content in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  prefix: 'tw-',
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
