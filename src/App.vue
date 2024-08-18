41b22217b01c324127d8e83b27451142e821c142
Jerald Bianan <jeraldbianan@gmail.com>
2024-08-18 17:54:37 +0800

    added loading indicator when user first visits the app

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,23 @@
-<template>
-  <router-view />
-</template>
-
 <script setup lang="ts">
+import { onMounted, ref } from 'vue';
+import LoadingIndicator from './components/UI/LoadingIndicator.vue';
+
 defineOptions({
   name: 'App',
 });
+
+const isLoading = ref(true);
+
+onMounted(() => {
+  setTimeout(() => {
+    isLoading.value = false;
+  }, 2000);
+});
 </script>
+
+<template>
+  <div>
+    <LoadingIndicator v-if="isLoading" />
+    <router-view v-else />
+  </div>
+</template>
---
+++ src/components/UI/LoadingIndicator.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="flex h-screen flex-col items-center justify-center gap-4">
+    <q-spinner-cube color="red" size="5.5em" />
+    <p class="text-xl">Preparing the site...</p>
+  </div>
+</template>
+
+<style scoped></style>
