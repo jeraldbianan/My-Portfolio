c3106ca6cf78ef03605e97462d2414ff13bf46bd
Jerald Bianan <jeraldbianan@gmail.com>
2024-08-18 17:26:30 +0800

    develop Mainheader component, and imported NavigationLinks as part of it

---
+++ src/layouts/MainHeader/NavigationLinks.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const links = ref([
+  {
+    title: 'Home',
+    url: '/',
+  },
+  {
+    title: 'Projects',
+    url: '/projects',
+  },
+  {
+    title: 'Contact',
+    url: '/contact',
+  },
+  {
+    title: 'About me',
+    url: '/aboutme',
+  },
+]);
+</script>
+
+<template>
+  <ul v-for="link in links" :key="link.title">
+    <router-link
+      :to="link.url"
+      class="font-bebas-neue rounded-lg p-4 text-lg"
+      >{{ link.title }}</router-link
+    >
+  </ul>
+</template>
+
+<style lang="scss" scoped>
+.router-link-active {
+  @apply bg-red-500 text-white;
+}
+</style>
