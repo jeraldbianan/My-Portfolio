a238e7ef8eed11d45d0a95ea84bef195c70e611d
Jerald Bianan <jeraldbianan@gmail.com>
2024-09-03 15:20:27 +0800

    develop the navigation bar

---
+++ src/layouts/NavigationBar.vue
@@ -0,0 +1,24 @@
+<template>
+  <q-header
+    class="mx-auto mt-5 flex max-w-container justify-between bg-transparent"
+  >
+    <div class="flex">
+      <q-img
+        src="src/assets/images/JB-logo.svg"
+        fit="none"
+        class="h-[68px] w-[165px]"
+      />
+    </div>
+    <div class="flex items-center gap-8">
+      <DarkModeToggle />
+      <NavigationLinks />
+    </div>
+  </q-header>
+</template>
+
+<script setup lang="ts">
+import DarkModeToggle from 'src/components/UI/DarkModeToggle.vue';
+import NavigationLinks from './MainHeader/NavigationLinks.vue';
+</script>
+
+<style lang="scss" scoped></style>
