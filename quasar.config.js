3f4bf690d81165febe38ca012c3f2e671178d9be
Jerald Bianan <jeraldbianan@gmail.com>
2024-09-03 14:01:57 +0800

    added font awesome in quasar config

--- quasar.config.js
+++ quasar.config.js
@@ -27,7 +27,7 @@ module.exports = configure(function (/* ctx */) {
     extras: [
       // 'ionicons-v4',
       // 'mdi-v7',
-      // 'fontawesome-v6',
+      'fontawesome-v6',
       // 'eva-icons',
       // 'themify',
       // 'line-awesome',
