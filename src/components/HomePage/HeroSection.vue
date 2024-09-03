1bcebee5d0fd3654d03dd3ba5b934f2741228811
Jerald Bianan <jeraldbianan@gmail.com>
2024-09-03 15:19:37 +0800

    develop hero section

---
+++ src/components/HomePage/HeroSection.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import IntroductionSection from './HeroSection/IntroductionSection.vue';
+</script>
+
+<template>
+  <div>
+    <q-img
+      src="src/assets/images/hero-background.jpg"
+      alt="Hero Background"
+      class="relative min-h-[800px]"
+    >
+      <div
+        class="absolute bottom-0 left-0 right-0 top-0 flex items-center justify-center !bg-black/50"
+      >
+        <div class="w-full max-w-container">
+          <IntroductionSection />
+        </div>
+      </div>
+    </q-img>
+  </div>
+</template>
+
+<style scoped></style>
