e875f8571d7d479efa075123a2ae9094b8f341b2
Jerald Bianan <jeraldbianan@gmail.com>
2024-09-03 16:01:28 +0800

    added technologies data

---
+++ src/data/technologies.ts
@@ -0,0 +1,44 @@
+export const technologies = [
+  {
+    id: 1,
+    name: 'Figma',
+    image: 'figma.svg',
+    url: 'https://www.figma.com/',
+  },
+  {
+    id: 2,
+    name: 'Webflow',
+    image: 'webflow.svg',
+    url: 'https://webflow.com/',
+  },
+  {
+    id: 3,
+    name: 'Javascript',
+    image: 'javascript.svg',
+    url: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript',
+  },
+  {
+    id: 4,
+    name: 'VueJS',
+    image: 'vue.svg',
+    url: 'https://vuejs.org/',
+  },
+  {
+    id: 5,
+    name: 'Quasar',
+    image: 'quasar.svg',
+    url: 'https://quasar.dev/',
+  },
+  {
+    id: 6,
+    name: 'Typescript',
+    image: 'typescript.svg',
+    url: 'https://www.typescriptlang.org/',
+  },
+  {
+    id: 7,
+    name: 'Tailwind CSS',
+    image: 'tailwind.svg',
+    url: 'https://tailwindcss.com/',
+  },
+];
