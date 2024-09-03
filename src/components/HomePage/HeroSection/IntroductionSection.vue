44d89520f0ee0fcbf60f29804189dd26e27ca898
Jerald Bianan <jeraldbianan@gmail.com>
2024-09-03 17:00:24 +0800

    added gsap animation in the introduction section of the hero section component

--- src/components/HomePage/HeroSection/IntroductionSection.vue
+++ src/components/HomePage/HeroSection/IntroductionSection.vue
@@ -1,24 +1,61 @@
 <template>
   <div class="max-w-[600px]">
-    <h1
-      class="font-poppins text-[65px] font-black leading-[67.5%] text-main-white"
+    <transition-group
+      appear
+      tag="div"
+      @before-enter="beforeEnter"
+      @enter="enter"
     >
-      I’m <span class="text-main-blue">Jerald Bianan</span>
-    </h1>
-    <p
-      class="mt-5 max-w-[350px] font-roboto text-[26px] font-normal leading-[150%]"
-    >
-      Front-end Web Developer and UI/UX Designer
-    </p>
-    <q-btn
-      unelevated
-      class="mt-10 h-16 w-full max-w-[270px] bg-main-blue font-roboto text-lg font-medium normal-case hover:bg-main-blue/80"
-    >
-      View Latest Projects
-    </q-btn>
+      <h1
+        :key="1"
+        class="font-poppins text-[65px] font-black leading-[67.5%] text-main-white"
+      >
+        I’m <span class="text-main-blue">Jerald Bianan</span>
+      </h1>
+
+      <p
+        :key="2"
+        class="mt-5 max-w-[350px] font-roboto text-[26px] font-normal leading-[150%]"
+      >
+        A Front-end Web Developer
+      </p>
+      <q-btn
+        :key="3"
+        unelevated
+        class="mt-10 h-16 w-full max-w-[270px] bg-main-blue font-roboto text-lg font-medium normal-case transition hover:bg-main-blue/80"
+      >
+        View Latest Projects
+      </q-btn>
+    </transition-group>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import gsap from 'gsap';
 
-<style scoped></style>
+const beforeEnter = (el: Element) => {
+  const element = el as HTMLElement;
+  element.style.transform = 'translateX(-60px)';
+  element.style.opacity = '0';
+};
+
+const enter = (el: Element) => {
+  const element = el as HTMLElement;
+  gsap.to(element, {
+    duration: 0.5,
+    x: 0,
+    opacity: 1,
+  });
+};
+</script>
+
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s ease;
+}
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
