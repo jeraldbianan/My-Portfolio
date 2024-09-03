8e9c71a8c22b17fec9eea3c1a735bb29f8f7ebc6
Jerald Bianan <jeraldbianan@gmail.com>
2024-09-03 15:19:27 +0800

    remove loading spinner in app for now.

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,22 @@
 <script setup lang="ts">
-import { onMounted, ref } from 'vue';
-import LoadingIndicator from './components/UI/LoadingIndicator.vue';
+// import LoadingSpinner from './components/UI/LoadingSpinner.vue';
 
 defineOptions({
   name: 'App',
 });
 
-const isLoading = ref(true);
+// const isLoading = ref(true);
 
-onMounted(() => {
-  setTimeout(() => {
-    isLoading.value = false;
-  }, 2000);
-});
+// onMounted(() => {
+//   setTimeout(() => {
+//     isLoading.value = false;
+//   }, 1000);
+// });
 </script>
 
 <template>
   <div>
-    <LoadingIndicator v-if="isLoading" />
-    <router-view v-else />
+    <!-- <LoadingSpinner v-if="isLoading" /> -->
+    <router-view />
   </div>
 </template>
