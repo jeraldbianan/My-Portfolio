812c783fe1da9ca16a4229c8a30fe64dda13c855
Jerald Bianan <jeraldbianan@gmail.com>
2024-09-03 15:20:15 +0800

    develop dark mode toggle component and added in the navbar

---
+++ src/components/UI/DarkModeToggle.vue
@@ -0,0 +1,15 @@
+<template>
+  <button
+    @click="toggleDarkMode"
+    class="hover:bg-dark-blue dark:hover:bg-main-yellow bg-main-grey h-12 w-12 rounded-full"
+  >
+    <i v-if="!darkMode" class="fa-regular fa-moon fa-xl pr-[1px] pt-[1px]"></i>
+    <i v-if="darkMode" class="fa-regular fa-sun fa-xl pr-[1px] pt-[1px]"></i>
+  </button>
+</template>
+
+<script setup lang="ts">
+import { useDarkMode } from 'src/composables/useDarkMode';
+
+const { toggleDarkMode, darkMode } = useDarkMode();
+</script>
