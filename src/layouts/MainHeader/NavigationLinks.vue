da9f2838cc36cda7ca83a8156527c4fdc7e3068c
Jerald Bianan <jeraldbianan@gmail.com>
2024-09-03 15:20:55 +0800

    develop the navigationlinks and imported in the navigation bar

--- src/layouts/MainHeader/NavigationLinks.vue
+++ src/layouts/MainHeader/NavigationLinks.vue
@@ -10,29 +10,28 @@ const links = ref([
     title: 'Projects',
     url: '/projects',
   },
-  {
-    title: 'Contact',
-    url: '/contact',
-  },
-  {
-    title: 'About me',
-    url: '/aboutme',
-  },
 ]);
 </script>
 
 <template>
-  <ul v-for="link in links" :key="link.title">
-    <router-link
-      :to="link.url"
-      class="font-bebas-neue rounded-lg p-4 text-lg"
-      >{{ link.title }}</router-link
+  <div class="flex items-center gap-8">
+    <ul v-for="link in links" :key="link.title">
+      <router-link
+        :to="link.url"
+        class="pb-2 font-roboto text-sm font-medium text-main-white hover:border-b-4 hover:border-b-main-blue"
+        >{{ link.title }}</router-link
+      >
+    </ul>
+    <q-btn
+      unelevated
+      class="h-[42px] w-[105px] bg-white/20 normal-case text-main-white"
+      >Contact</q-btn
     >
-  </ul>
+  </div>
 </template>
 
 <style lang="scss" scoped>
 .router-link-active {
-  @apply bg-red-500 text-white;
+  @apply border-b-4 border-b-main-blue;
 }
 </style>
