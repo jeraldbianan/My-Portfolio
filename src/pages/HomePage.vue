f43612b92970f9dfd59e04eaa5960b37069b8cd5
Jerald Bianan <jeraldbianan@gmail.com>
2024-09-03 17:01:26 +0800

    import aboutme component in HomePage

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,7 +1,11 @@
-<script setup lang="ts"></script>
-
 <template>
-  <div>Home</div>
+  <div>
+    <AboutMe />
+  </div>
 </template>
+
+<script setup lang="ts">
+import AboutMe from 'src/components/HomePage/AboutMe.vue';
+</script>
 
 <style scoped></style>
