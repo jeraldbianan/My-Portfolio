042732a479b6da95349691809be66b90fced9a28
Jerald Bianan <jeraldbianan@gmail.com>
2024-09-03 15:19:54 +0800

    develop the introduction section for the hero section

---
+++ src/components/HomePage/HeroSection/IntroductionSection.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="max-w-[600px]">
+    <h1
+      class="font-poppins text-[65px] font-black leading-[67.5%] text-main-white"
+    >
+      I’m <span class="text-main-blue">Jerald Bianan</span>
+    </h1>
+    <p
+      class="mt-5 max-w-[350px] font-roboto text-[26px] font-normal leading-[150%]"
+    >
+      Front-end Web Developer and UI/UX Designer
+    </p>
+    <q-btn
+      unelevated
+      class="mt-10 h-16 w-full max-w-[270px] bg-main-blue font-roboto text-lg font-medium normal-case hover:bg-main-blue/80"
+    >
+      View Latest Projects
+    </q-btn>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
