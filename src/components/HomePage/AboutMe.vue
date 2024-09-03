922536db0d6c6f0482e6012f3b7b7ba7e4990b25
Jerald Bianan <jeraldbianan@gmail.com>
2024-09-03 17:00:49 +0800

    added about me component

---
+++ src/components/HomePage/AboutMe.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="flex justify-center bg-white py-[90px] dark:!bg-dark-blue">
+    <div class="flex w-full max-w-container flex-col items-center">
+      <h2
+        class="font-poppins text-[45px] font-normal text-dark-blue dark:text-main-white"
+      >
+        About Me
+      </h2>
+
+      <div class="mt-[90px] flex w-full justify-between">
+        <div
+          class="flex w-full max-w-[567px] flex-col gap-8 font-roboto text-2xl font-normal leading-[145.8%] text-main-grey dark:text-main-white"
+        >
+          <p>
+            In front-end web development I have 2+ years of experience, I
+            primarily use VueJS, Quasar, and Tailwind CSS to build responsive
+            and dynamic front-end experiences.
+          </p>
+          <p>
+            In the field of UI/UX design, I work with Figma and Webflow to
+            create intuitive and engaging user interfaces.
+          </p>
+        </div>
+        <TechnologiesComponent />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import TechnologiesComponent from './AboutMe/TechnologiesComponent.vue';
+</script>
+
+<style scoped></style>
