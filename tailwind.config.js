865c07ab2ab3adebd3162feec8fe9bf44d6d0fc7
Jerald Bianan <jeraldbianan@gmail.com>
2024-09-03 13:42:11 +0800

    added fonts, colors, dark mode in tailwind config that is required for the project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: [
     './index.html',
@@ -6,16 +7,23 @@ module.exports = {
     './node_modules/quasar/src/**/*.{js,ts,vue}',
     './node_modules/quasar/src/**/*.sass',
   ],
+  darkMode: 'class',
   theme: {
     extend: {
+      colors: {
+        blue: '#0BBEF2',
+        'dark-blue': '#324162',
+        grey: '#697694',
+        'dark-grey': '#3C4862',
+        yellow: '#FFB200',
+        white: '#F5F7FA',
+      },
       fontFamily: {
         poppins: ['Poppins', 'sans-serif'],
         roboto: ['Roboto', 'sans-serif'],
       },
-      container: {
-        screens: {
-          main: '1160px',
-        },
+      maxWidth: {
+        container: '1160px',
       },
     },
   },
