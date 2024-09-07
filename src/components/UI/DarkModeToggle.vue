150a515d94dd1ed3373ffe0d9124ce65c335a5c7
Jerald Bianan <jeraldbianan@gmail.com>
2024-09-07 19:33:52 +0800

    refactor dark mode and added quasar notify

--- src/components/UI/DarkModeToggle.vue
+++ src/components/UI/DarkModeToggle.vue
@@ -1,7 +1,7 @@
 <template>
   <button
     @click="toggleDarkMode"
-    class="hover:bg-dark-blue dark:hover:bg-main-yellow bg-main-grey h-12 w-12 rounded-full"
+    class="h-12 w-12 rounded-full bg-dark-blue transition-all hover:scale-110 dark:bg-main-yellow"
   >
     <i v-if="!darkMode" class="fa-regular fa-moon fa-xl pr-[1px] pt-[1px]"></i>
     <i v-if="darkMode" class="fa-regular fa-sun fa-xl pr-[1px] pt-[1px]"></i>
--- src/composables/useDarkMode.ts
+++ src/composables/useDarkMode.ts
@@ -1,3 +1,4 @@
+import { Notify } from 'quasar';
 import { ref } from 'vue';
 
 const darkMode = ref<boolean>(localStorage.getItem('darkMode') === 'true');
@@ -6,6 +7,12 @@ function toggleDarkMode() {
   darkMode.value = !darkMode.value;
   localStorage.setItem('darkMode', darkMode.value.toString());
   document.documentElement.classList.toggle('dark', darkMode.value);
+  Notify.create({
+    message: darkMode.value ? 'Dark Mode Enabled.' : 'Light Mode Enabled.',
+    position: 'bottom',
+    timeout: 500,
+    classes: 'notify',
+  });
 }
 
 if (darkMode.value) {
