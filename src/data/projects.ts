b1454157857bcaf61996e7262e0001f62d846818
Jerald Bianan <jeraldbianan@gmail.com>
2024-09-08 11:19:59 +0800

    added image resource and added the technologies data

---
+++ src/data/projects.ts
@@ -0,0 +1,12 @@
+export const projects = [
+  {
+    id: 1,
+    title: 'JB Blogs',
+    image: 'jb-blogs.jpg',
+    mobileImg: 'jb-blogs-mobile.png',
+    description:
+      'A simple responsive blog featuring VueJS and Firebase, Authentication and CRUD functionality is applied.',
+    technologies: ['javascript', 'vue', 'quasar', 'typescript'],
+    url: 'https://jb-blogs.netlify.app/',
+  },
+];
