24fa7b2b3560c3055b22e3f189e7ea4957f9a79c
Jerald Bianan <jeraldbianan@gmail.com>
2024-09-08 14:13:54 +0800

    added gsap animation in about me

--- src/components/HomePage/AboutMe.vue
+++ src/components/HomePage/AboutMe.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="flex justify-center bg-white py-[90px] dark:!bg-dark-blue">
+  <div
+    ref="aboutMe"
+    class="flex justify-center bg-white py-[90px] dark:!bg-dark-blue"
+  >
     <div class="flex w-full max-w-container flex-col items-center">
       <h2
         class="font-poppins text-[45px] font-normal text-dark-blue dark:text-main-white"
@@ -7,7 +10,7 @@
         About Me
       </h2>
 
-      <div class="mt-[90px] flex w-full justify-between">
+      <div class="about-me mt-[90px] flex w-full justify-between">
         <div
           class="flex w-full max-w-[567px] flex-col gap-8 font-roboto text-2xl font-normal leading-[145.8%] text-main-grey dark:text-main-white"
         >
@@ -28,7 +31,36 @@
 </template>
 
 <script setup lang="ts">
+import { onMounted, ref } from 'vue';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
 import TechnologiesComponent from './AboutMe/TechnologiesComponent.vue';
+
+gsap.registerPlugin(ScrollTrigger);
+
+const aboutMe = ref(null);
+
+onMounted(() => {
+  gsap.fromTo(
+    '.about-me',
+    {
+      y: 100,
+      opacity: 0,
+    },
+    {
+      y: 0,
+      opacity: 1,
+      duration: 1,
+      stagger: 0.3,
+      scrollTrigger: {
+        trigger: aboutMe.value,
+        start: 'top 80%',
+        toggleActions: 'play none none none',
+      },
+    },
+  );
+});
 </script>
 
 <style scoped></style>
