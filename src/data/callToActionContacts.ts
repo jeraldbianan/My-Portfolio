608d6e1cbffef1850a8d2c7b9dd063c49c061196
Jerald Bianan <jeraldbianan@gmail.com>
2024-09-08 14:14:25 +0800

    added callToActionContacts data

---
+++ src/data/callToActionContacts.ts
@@ -0,0 +1,26 @@
+export const callToActionContacts = [
+  {
+    id: 1,
+    name: 'Upwork',
+    iconSrc: 'upwork.svg',
+    url: 'https://www.upwork.com/freelancers/~01e654a15202196e2a?s=1044578476142100545',
+  },
+  {
+    id: 2,
+    name: 'LinkedIn',
+    iconSrc: 'linkedin.svg',
+    url: 'https://www.linkedin.com/in/jerald-bianan-865059254/',
+  },
+  {
+    id: 3,
+    name: 'Github Dark',
+    iconSrc: 'github-dark.svg',
+    url: 'https://github.com/jeraldbianan',
+  },
+  {
+    id: 4,
+    name: 'Github Light',
+    iconSrc: 'github-light.svg',
+    url: 'https://github.com/jeraldbianan',
+  },
+];
