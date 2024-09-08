8657a75be0e392e7d23c180e3a5657fc832fdeb7
Jerald Bianan <jeraldbianan@gmail.com>
2024-09-08 11:20:55 +0800

    added projectsSection in Homepage

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,11 +1,13 @@
 <template>
   <div>
     <AboutMe />
+    <ProjectsSection />
   </div>
 </template>
 
 <script setup lang="ts">
 import AboutMe from 'src/components/HomePage/AboutMe.vue';
+import ProjectsSection from 'src/components/HomePage/ProjectsSection.vue';
 </script>
 
 <style scoped></style>
