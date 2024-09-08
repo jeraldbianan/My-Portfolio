f773fcf14f64cec875f420df2aa6df71ef256488
Jerald Bianan <jeraldbianan@gmail.com>
2024-09-08 14:14:37 +0800

    import CallToAction in Home page

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -2,11 +2,13 @@
   <div>
     <AboutMe />
     <ProjectsSection />
+    <CallToAction />
   </div>
 </template>
 
 <script setup lang="ts">
 import AboutMe from 'src/components/HomePage/AboutMe.vue';
+import CallToAction from 'src/components/HomePage/CallToAction.vue';
 import ProjectsSection from 'src/components/HomePage/ProjectsSection.vue';
 </script>
 
