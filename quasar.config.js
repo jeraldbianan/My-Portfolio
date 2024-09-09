54c2ebff356384277ca0ce5b08dc2dc645a59794
Jerald Bianan <jeraldbianan@gmail.com>
2024-09-09 13:34:29 +0800

    remove public path

--- quasar.config.js
+++ quasar.config.js
@@ -51,7 +51,7 @@ module.exports = configure(function (/* ctx */) {
 
       // rebuildCache: true, // rebuilds Vite/linter/etc cache on startup
 
-      publicPath: '/My-Portfolio/',
+      // publicPath: '/My-Portfolio/',
       // analyze: true,
       // env: {},
       // rawDefine: {}
