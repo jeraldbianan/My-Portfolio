4eac2e0700235d6c3d7d2e75fb3b50b9d8dee9aa
Jerald Bianan <jeraldbianan@gmail.com>
2024-09-09 08:06:22 +0800

    apply loading animation if site is iniatially loaded

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,23 @@
 <script setup lang="ts">
-// import LoadingSpinner from './components/UI/LoadingSpinner.vue';
+import { onMounted, ref } from 'vue';
+import LoadingSpinner from './components/UI/LoadingSpinner.vue';
 
 defineOptions({
   name: 'App',
 });
 
-// const isLoading = ref(true);
+const isLoading = ref(true);
 
-// onMounted(() => {
-//   setTimeout(() => {
-//     isLoading.value = false;
-//   }, 1000);
-// });
+onMounted(() => {
+  setTimeout(() => {
+    isLoading.value = false;
+  }, 1000);
+});
 </script>
 
 <template>
   <div>
-    <!-- <LoadingSpinner v-if="isLoading" /> -->
-    <router-view />
+    <LoadingSpinner v-if="isLoading" />
+    <router-view v-else />
   </div>
 </template>
