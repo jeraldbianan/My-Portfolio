2b45522f57b19e64bba0452917a52cb84911cdc9
Jerald Bianan <jeraldbianan@gmail.com>
2024-09-09 13:29:11 +0800

    added deploy script

--- quasar.config.js
+++ quasar.config.js
@@ -51,7 +51,7 @@ module.exports = configure(function (/* ctx */) {
 
       // rebuildCache: true, // rebuilds Vite/linter/etc cache on startup
 
-      // publicPath: '/',
+      publicPath: '/My-Portfolio/',
       // analyze: true,
       // env: {},
       // rawDefine: {}
