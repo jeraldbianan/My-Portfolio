96c9d5b20db204a023dbff76a0973985274c2a4c
Jerald Bianan <jeraldbianan@gmail.com>
2024-10-24 16:11:38 +0800

    remove upwork in contacts

--- src/data/callToActionContacts.ts
+++ src/data/callToActionContacts.ts
@@ -1,24 +1,24 @@
 export const callToActionContacts = [
+  // {
+  //   id: 1,
+  //   name: 'Upwork',
+  //   iconSrc: 'upwork.svg',
+  //   url: 'https://www.upwork.com/freelancers/~01e654a15202196e2a?s=1044578476142100545',
+  // },
   {
     id: 1,
-    name: 'Upwork',
-    iconSrc: 'upwork.svg',
-    url: 'https://www.upwork.com/freelancers/~01e654a15202196e2a?s=1044578476142100545',
-  },
-  {
-    id: 2,
     name: 'LinkedIn',
     iconSrc: 'linkedin.svg',
     url: 'https://www.linkedin.com/in/jerald-bianan-865059254/',
   },
   {
-    id: 3,
+    id: 2,
     name: 'Github Dark',
     iconSrc: 'github-dark.svg',
     url: 'https://github.com/jeraldbianan',
   },
   {
-    id: 4,
+    id: 3,
     name: 'Github Light',
     iconSrc: 'github-light.svg',
     url: 'https://github.com/jeraldbianan',
