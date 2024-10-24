7fee23b2d240d59364c6b8fc1f10309d7b03f6d5
Jerald Bianan <jeraldbianan@gmail.com>
2024-10-24 13:05:25 +0800

    change introduction in about me

--- src/components/HomePage/AboutMe.vue
+++ src/components/HomePage/AboutMe.vue
@@ -1,22 +1,22 @@
 <template>
   <div
-    class="mobile:py-[60px] flex justify-center bg-white py-[90px] dark:!bg-dark-blue"
+    class="flex justify-center bg-white py-[90px] dark:!bg-dark-blue mobile:py-[60px]"
   >
     <div
-      class="mobile:mx-[30px] mx-[60px] flex w-full max-w-container flex-col items-center"
+      class="mx-[60px] flex w-full max-w-container flex-col items-center mobile:mx-[30px]"
     >
       <h2
-        class="mobile:text-3xl font-poppins text-[45px] font-normal text-dark-blue dark:text-main-white"
+        class="font-poppins text-[45px] font-normal text-dark-blue dark:text-main-white mobile:text-3xl"
       >
         About Me
       </h2>
 
       <div
         ref="aboutMe"
-        class="tablet:mt-[60px] mobile:mt-[30px] tablet:flex-col tablet:items-center mt-[90px] flex w-full justify-between"
+        class="mt-[90px] flex w-full justify-between tablet:mt-[60px] tablet:flex-col tablet:items-center mobile:mt-[30px]"
       >
         <div
-          class="intro tablet:max-w-full mobile:text-lg flex w-full max-w-[567px] flex-col gap-8 font-roboto text-2xl font-normal leading-[145.8%] text-main-grey dark:text-main-white"
+          class="intro flex w-full max-w-[567px] flex-col gap-8 font-roboto text-2xl font-normal leading-[145.8%] text-main-grey dark:text-main-white tablet:max-w-full mobile:text-lg"
         >
           <p>
             In front-end web development I have 2+ years of experience, I
@@ -24,8 +24,12 @@
             and dynamic front-end experiences.
           </p>
           <p>
-            In the field of UI/UX design, I work with Figma and Webflow to
-            create intuitive and engaging user interfaces.
+            In the backend, I use PHP and Laravel to build robust and scalable
+            applications. I am proficient in designing RESTful APIs and managing
+            database interactions using Eloquent ORM. My expertise in Laravel
+            allows me to implement clean architecture, follow best coding
+            practices, and ensure the application is secure, efficient, and
+            maintainable.
           </p>
         </div>
         <TechnologiesComponent class="techonologies tablet:mt-[60px]" />
